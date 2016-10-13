da600f4741fb7c7b9d7b339574c485d66fae0171
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2016-10-13 12:13:11 +0100

    Added way to sort the dois per year and per last number

--- extract_dois.py
+++ extract_dois.py
@@ -12,10 +12,11 @@ class DOIExtractor():
     '''
     Extract the list of DOIs
     '''
-    def extract(self, directory, order):
+    def extract(self, directory, order_peryear_pernumber):
         isa_dirs = os.listdir(directory)
 
-        dois_string = ""
+        dois = []
+        dois_dict = dict()
 
         for count, isa_dir in enumerate(isa_dirs):
             print(isa_dir)
@@ -40,12 +41,33 @@ class DOIExtractor():
                         study_identifier = study_record.metadata['Study Identifier']
                         doi_url = "doi:" + study_identifier
 
-                        dois_string = dois_string + doi_url + self.new_line
+                        if (order_peryear_pernumber):
+                            dot = '.'
+                            dots_positions = [ pos for pos, char in enumerate(doi_url) if char == dot ]
+                            index_dot_before_year =  dots_positions[ len(dots_positions) -2 ] + 1
+                            index_dot_after_year = dots_positions[ len(dots_positions) -1 ]
+                            year = int( doi_url [ index_dot_before_year : index_dot_after_year])
+                            last_number = int (doi_url [ (index_dot_after_year +1 ):])
+                            year_dict = dois_dict.get(year)
+                            if (year_dict == None):
+                                year_dict = dict()
+                            year_dict.update({ last_number :  doi_url })
+                            dois_dict.update({ year : year_dict})
+                        else:
+                            dois.append(doi_url)
 
-
-        output_file = open('scidata_dois.tsv', 'w')
-        output_file.write(dois_string)
-        output_file.close()
+        if (order_peryear_pernumber):
+            output_file = open('scidata_dois_ordered.tsv', 'w')
+            for doi_year in sorted(dois_dict.keys()):
+                for doi_last_number in sorted( dois_dict.get(doi_year).keys() ):
+                    output_file.write(dois_dict.get(doi_year).get(doi_last_number) + self.new_line)
+            output_file.close()
+        else:
+            dois.sort()
+            output_file = open('scidata_dois.tsv', 'w')
+            for item in dois:
+                output_file.write(item + self.new_line)
+            output_file.close()
 
 
 if __name__ == "__main__":
