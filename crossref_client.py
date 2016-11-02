5dc4e458298531f7e0e1d4902d1c4cc4b31c5a83
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2016-11-02 17:23:57 +0000

    getting info from crossref for sdata download  #42

--- crossref_client.py
+++ crossref_client.py
@@ -8,24 +8,32 @@ class CrossRefCient:
     CROSS_REF_API_BASE_URL = "http://api.crossref.org"
     TIMEOUT = 5
 
-    def getWorksBy(self, issn):
-        r = requests.get(urljoin(self.CROSS_REF_API_BASE_URL, "/journals/"+issn+"/works?rows=1000"), timeout=self.TIMEOUT)
-        print(r.text)
-
-
-    def getWorks(self):
-        r = requests.get(urljoin(self.CROSS_REF_API_BASE_URL,"/works"), timeout=self.TIMEOUT)
-        print(r.text)
-
-    def getFunders(self):
-        r = requests.get(urljoin(self.CROSS_REF_API_BASE_URL, "/funders"), timeout=self.TIMEOUT)
-        print(r.text)
+    def getURLPiecesWorksByScientificData(self):
+        ScientificDataISSN = "2052-4463"
+        r = requests.get(urljoin(self.CROSS_REF_API_BASE_URL, "/journals/"+ScientificDataISSN+"/works?rows=1000"), timeout=self.TIMEOUT)
+        json_result = r.json()
+        total_results = json_result["message"]["total-results"]
+        items = json_result["message"]["items"]
+        print("total_results ", total_results)
+        url_pieces = []
+        for item in items:
+            sdata_identifer = item["alternative-id"][0]
+            accepted_year = sdata_identifer[5:9]
+            published_year = item["deposited"]["date-parts"]
+            print("sdata_identifer  ", sdata_identifer)
+            print("accepted_year ", accepted_year)
+            print("published_year ", published_year)
 
 
 if __name__ == "__main__":
     client = CrossRefCient()
-    ScientificDataISSN = "2052-4463"
+    client.getURLPiecesWorksByScientificData()
 
-    #client.getWorks()
-    #client.getFunders()
-    client.getWorksByJournal(ScientificDataISSN)
+    #url_pieces = (published_year,accepted_year,current_id)
+    #url = 'http://www.nature.com/article-assets/npg/sdata/{0}/sdata{1}{2}/isa-tab/sdata{1}{2}-isa1.zip'.format(*url_pieces)
+
+
+#example
+#dd: www.nature.com/articles/sdata201575 --> accepted year
+# deposited 2016  --> published year
+#  http://www.nature.com/article-assets/npg/sdata/2016/sdata201575/isa-tab/sdata201575-isa1.zip
