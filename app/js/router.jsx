88c7268c6a386e0a3f7454582d0d8e5f127a102e
zigur <massimorgon@gmail.com>
2017-02-17 16:36:48 +0000

    fixed page scroll issue

--- app/js/router.jsx
+++ app/js/router.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, applyRouterMiddleware, browserHistory } from 'react-router';
+import { useScroll } from 'react-router-scroll';
 import StudiesContainer from './components/containers/studies-container';
 import StudyContainer from './components/containers/study-container';
 import TableRendererContainer from './components/containers/tableRenderer-container';
 
+
 export default (
-    <Router history={browserHistory}>
+    <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory}>
         <Route path='/'>
             <IndexRoute component={StudiesContainer} />
             <Route path=':dirName' component={StudyContainer} ></Route>
