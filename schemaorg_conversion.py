a23ace3c9825a5f9289df3150009f9cbb09936a4
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2017-03-06 17:08:56 +0000

    Update to script

--- schemaorg_conversion.py
+++ schemaorg_conversion.py
@@ -17,7 +17,7 @@ def convert(isatab_ref):
     assert os.path.exists(isatab_ref), "Did not find investigation file: %s" % isatab_ref
 
     with open(os.path.join(isatab_ref)) as fp:
-        isa_tab = isatab.load(fp)
+        isa_tab = isatab.load(fp, skip_load_tables=True)
 
         study = isa_tab.studies[0]
         dataset.update( {"name": study.title} )
@@ -38,11 +38,11 @@ def convert(isatab_ref):
         dataset.update({ "citation": study.identifier} )
 
         ### data records
-        for c in study.comments:
-            print("c ---> " , c.name , "    ", c.value)
+        #for c in study.comments:
+        #    print("c ---> " , c.name , "    ", c.value)
 
-        for c in study.contacts[0].comments:
-            print("c ---> ", c.name, "    ", c.value)
+        #for c in study.contacts[0].comments:
+        #    print("c ---> ", c.name, "    ", c.value)
 
 
     return dataset
@@ -58,8 +58,9 @@ if __name__ == "__main__":
 
     isa_dirs = os.listdir(data_path)
     for count, isa_dir in enumerate(isa_dirs):
-        if os.path.isdir(os.path.join(data_path, isa_dir)) and (isa_dir != "jsonld"):
+        if (os.path.isdir(os.path.join(data_path, isa_dir)) and (isa_dir != "jsonld")):
             isa_dir_full_path = os.path.join(data_path, isa_dir)
+            print("Converting "+isa_dir_full_path)
             dataset_json = convert(isa_dir_full_path)
             output_file = os.path.join( os.path.join(data_path, "jsonld/") + isa_dir + '.json')
             with open(output_file, 'w') as outfile:
