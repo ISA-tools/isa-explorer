ba241f30010acb8daaf41c5f4af40b39ff3e6bb3
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2017-03-06 14:03:07 +0000

    More info for conversion

--- schemaorg_conversion.py
+++ schemaorg_conversion.py
@@ -23,6 +23,7 @@ def convert(isatab_ref):
         dataset.update( {"name": study.title} )
         dataset.update( { "description": study.description} )
 
+        ### creators
         creators = []
         for contact in study.contacts:
             person = {
@@ -34,10 +35,33 @@ def convert(isatab_ref):
 
         dataset.update({ "creator": creators })
 
+        dataset.update({ "citation": study.identifier} )
+
+        ### data records
+        for c in study.comments:
+            print("c ---> " , c.name , "    ", c.value)
+
+        for c in study.contacts[0].comments:
+            print("c ---> ", c.name, "    ", c.value)
+
+
     return dataset
 
 
 if __name__ == "__main__":
-    dataset_json = convert("./data/sdata20141")
-    with open('dataset.json', 'w') as outfile:
-        json.dump(dataset_json, outfile)
+    data_path = os.path.abspath("./data")
+
+    try:
+        os.stat(os.path.join(data_path, "jsonld"))
+    except:
+        os.mkdir(os.path.join(data_path, "jsonld"))
+
+    isa_dirs = os.listdir(data_path)
+    for count, isa_dir in enumerate(isa_dirs):
+        if os.path.isdir(os.path.join(data_path, isa_dir)) and (isa_dir != "jsonld"):
+            dataset_json = convert(os.path.join(data_path, isa_dir))
+            output_file = os.path.join( os.path.join(data_path, "jsonld/") + isa_dir + '.json')
+            with open(output_file, 'w') as outfile:
+                 json.dump(dataset_json, outfile)
+        else:
+            print(os.path.join(data_path, isa_dir), "not a dir")
