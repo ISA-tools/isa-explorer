589bc5bb8cc1777657e8e9e4650ff35a4f5abd9d
zigur <massimorgon@gmail.com>
2017-04-13 18:10:28 +0100

    implemented bash script #16

---
+++ scripts/load-data.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+clear
+
+cd /var/www/isaexplorer
+
+echo "Current directory: $PWD"
+
+echo "Stopping the server..."
+forever stop server.js
+
+FELIST="${forever list}"
+echo "${FELIST}"
+
+source venv-3.5/bin/activate
+
+echo "Downloading data via crossref..."
+python scripts/sdata_crossref_download.py
+
+echo "Building the index..."
+python scripts/build_index.py data
+
+echo "Restarting the server..."
+NODE_ENV=production PORT=80 forever start server.js
