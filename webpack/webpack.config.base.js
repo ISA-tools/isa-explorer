477b22ec0ccd7594c9e90a713c711fa0cf7d04be
zigur <massimorgon@gmail.com>
2017-05-17 14:50:29 +0100

    removed unused dependency

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -3,7 +3,6 @@
  */
 const path = require('path');
 const webpack = require('webpack');
-const BundleTracker = require('webpack-bundle-tracker');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
