413ae4ee07085efc0704b5e36f337b24eb1a720e
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2017-10-04 11:52:06 +0100

    Adding lastmod to sitemap

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -1,6 +1,19 @@
 const sm = require('sitemap');
 const fs = require('fs'), path = require('path');
 
+Date.prototype.yyyymmdd = function() {
+  var mm = this.getMonth() + 1; // getMonth() is zero-based
+  var dd = this.getDate();
+
+  return [this.getFullYear(),
+          (mm>9 ? '' : '0') + mm,
+          (dd>9 ? '' : '0') + dd
+         ].join('');
+};
+var date = new Date();
+date.yyyymmdd();
+
+
 /**
  * @return{sitemap} sitemap object
  */
@@ -12,14 +25,14 @@ const generateSitemap = function generateSitemap({
 } = {}) {
 
     const sitemap = sm.createSitemap({hostname, cacheTime, urls: [
-        { url: '/', priority: 1}
+        { url: '/', priority: 1, lastmod: date}
     ]});
 
     const srcPath = path.join(__dirname, '..', dataDir), files = fs.readdirSync(srcPath);
 
     for (const file of files) {
         if (fs.statSync(path.join(srcPath, file)).isDirectory() && file.startsWith(pattern)) {
-            sitemap.add({url: `/${file}`, priority: 0.8});
+            sitemap.add({url: `/${file}`, priority: 0.8, lastmod: date});
         }
     }
     // fs.writeFileSync(path.join('assets', 'sitemap.xml'), sitemap.toString());
