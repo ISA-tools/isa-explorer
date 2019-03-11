34008af7fc75da2bccfe96d81861d15ff15d856a
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2019-03-11 16:11:47 +0000

    Storing ontology report

--- scripts/build_ontologies_report.py
+++ scripts/build_ontologies_report.py
@@ -2,14 +2,17 @@ __author__ = 'agbeltran'
 
 import os
 import glob
+import json
 from isatools.io.isatab_parser import InvestigationParser
 
 
 class OntologyUsageInfo():
     """
         Builds a report about the usage of ontologies
-        <ontology term> <data descriptor doi> <publication date>
+        <ontology source> <ontology term> <ontology label> <data descriptor doi> <publication date>
     """
+
+    ontology_info = {}
 
     design_type_field = "design_type"
     design_type_label = "Study Design Type"
@@ -44,6 +47,21 @@ class OntologyUsageInfo():
         }
     }
 
+    def add_info(self, ontology, ontology_label, ontology_URI, study_doi):
+        term_doi_dict = self.ontology_info.get(ontology)
+
+        if (term_doi_dict == None):
+            term_doi_dict = {}
+            self.ontology_info.update({ontology: term_doi_dict})
+
+        doi_list = term_doi_dict.get(ontology_URI)
+        if (doi_list == None):
+            doi_list = []
+
+        doi_list.append(study_doi)
+        term_doi_dict.update({ontology_URI : doi_list})
+
+
     def ontology_term_usage_report(self, data_directory, field):
         """
         Generates a report on ontology usage, given the field for which the annotation was done.
@@ -52,12 +70,9 @@ class OntologyUsageInfo():
         :param field: a string that can be design_type, measurement_type or technology_type
         """
 
-        repository_data = {}
-        repository_data_string = ""
-
         label_field = self.field_switcher.get(field, "Not found").get("label", "Not Found")
         term_field = self.field_switcher.get(field, "Not found").get("term", "Not Found")
-        ontology_field = self.field_switcher.get(field, "Not found").get("term", "Not Found")
+        ontology_field = self.field_switcher.get(field, "Not found").get("ontology", "Not Found")
 
         isa_dirs = os.listdir(data_directory)
         for count, isa_dir in enumerate(isa_dirs):
@@ -97,10 +112,12 @@ class OntologyUsageInfo():
                                 ontology_label = study_record.design_descriptors[i][label_field]
                                 ontology_URI = study_record.design_descriptors[i][term_field]
                                 ontology = study_record.design_descriptors[i][ontology_field]
+                                i = i+1
+                                # print("ontology_label ", ontology_label)
+                                # print("ontology_URI ", ontology_URI)
+                                # print("ontology ", ontology)
 
-                                print("ontology_label ", ontology_label)
-                                print("ontology_URI ", ontology_URI)
-                                print("ontology ", ontology)
+                                self.add_info(ontology, ontology_label, ontology_URI, doi_url)
 
                         elif (field == self.measurement_type_field or field == self.technology_type_field ):
                             i = 0
@@ -108,11 +125,15 @@ class OntologyUsageInfo():
                                 ontology_label = study_record.assays[i][label_field]
                                 ontology_URI = study_record.assays[i][term_field]
                                 ontology = study_record.assays[i][ontology_field]
+                                i = i + 1
+                                # print("ontology_label ", ontology_label)
+                                # print("ontology_URI ", ontology_URI)
+                                # print("ontology ", ontology)
 
-                                print("ontology_label ", ontology_label)
-                                print("ontology_URI ", ontology_URI)
-                                print("ontology ", ontology)
+                                self.add_info(ontology, ontology_label, ontology_URI, doi_url)
 
+        with open('ontology_info.json', 'w') as outfile:
+            json.dump(self.ontology_info, outfile)
         return;
 
 
