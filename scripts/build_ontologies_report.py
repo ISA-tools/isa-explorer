8196a28d6ad56eedaa8ebd7e39e1dbd307afa8a4
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2019-03-11 16:50:21 +0000

    Adding exception handling

--- scripts/build_ontologies_report.py
+++ scripts/build_ontologies_report.py
@@ -101,38 +101,35 @@ class OntologyUsageInfo():
                         study_identifier = study_record.metadata['Study Identifier']
                         doi_url = "http://dx.doi.org/" + study_identifier
 
-                        ontology_label = ""
-                        ontology_URI = ""
-                        ontology = ""
-
-                        if (field == self.design_type_field):
+                        if field == self.design_type_field:
 
                             i = 0
                             while i < len(study_record.design_descriptors):
-                                ontology_label = study_record.design_descriptors[i][label_field]
-                                ontology_URI = study_record.design_descriptors[i][term_field]
-                                ontology = study_record.design_descriptors[i][ontology_field]
-                                i = i+1
-                                # print("ontology_label ", ontology_label)
-                                # print("ontology_URI ", ontology_URI)
-                                # print("ontology ", ontology)
+                                try:
+                                    ontology_label = study_record.design_descriptors[i][label_field]
+                                    ontology_URI = study_record.design_descriptors[i][term_field]
+                                    ontology = study_record.design_descriptors[i][ontology_field]
+                                    if ontology != "" and ontology_URI != "":
+                                        self.add_info(ontology, ontology_label, ontology_URI, doi_url)
+                                except KeyError as ex:
+                                    print("The key doesn't exist! ", ex)
+                                i = i + 1
 
-                                self.add_info(ontology, ontology_label, ontology_URI, doi_url)
-
-                        elif (field == self.measurement_type_field or field == self.technology_type_field ):
+                        elif field == self.measurement_type_field or field == self.technology_type_field :
                             i = 0
                             while i < len(study_record.assays):
-                                ontology_label = study_record.assays[i][label_field]
-                                ontology_URI = study_record.assays[i][term_field]
-                                ontology = study_record.assays[i][ontology_field]
+                                try:
+                                    ontology_label = study_record.assays[i][label_field]
+                                    ontology_URI = study_record.assays[i][term_field]
+                                    ontology = study_record.assays[i][ontology_field]
+                                    if ontology != "" and ontology_URI != "":
+                                        self.add_info(ontology, ontology_label, ontology_URI, doi_url)
+                                except KeyError as ex:
+                                    print("The key doesn't exist! ", ex)
                                 i = i + 1
-                                # print("ontology_label ", ontology_label)
-                                # print("ontology_URI ", ontology_URI)
-                                # print("ontology ", ontology)
 
-                                self.add_info(ontology, ontology_label, ontology_URI, doi_url)
 
-        with open('ontology_info.json', 'w') as outfile:
+        with open(field+'_ontology_info.json', 'w') as outfile:
             json.dump(self.ontology_info, outfile)
         return;
 
@@ -142,3 +139,5 @@ if __name__ == "__main__":
         reporter = OntologyUsageInfo()
         #reporter.ontology_term_usage_report(sys.argv[1])
         reporter.ontology_term_usage_report("../data", "design_type")
+        reporter.ontology_term_usage_report("../data", "measurement_type")
+        reporter.ontology_term_usage_report("../data", "technology_type")
