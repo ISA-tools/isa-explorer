a51ca4177229ec5311958a11160568fd5c8bfbdb
zigur <massimorgon@gmail.com>
2019-08-27 16:15:16 +0200

    updated mainscript

--- scripts/load-data.sh
+++ scripts/load-data.sh
@@ -15,7 +15,8 @@ echo "${FELIST}"
 source venv-3.5/bin/activate
 
 echo "Downloading data via crossref..."
-python scripts/sdata_crossref_download.py
+# python scripts/sdata_crossref_download.py
+python scripts/sdata_crossref_full-live.py
 
 echo "Building the index..."
 python scripts/build_index.py data
